bbc88a5b41d32f50bb030cbefb6474eb43f29d62
Cody Beumer <cbeumer0428@gmail.com>
2023-03-08 17:15:40 -0600

    updating icons to svgs and removing in progress sections

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,6 @@ export default class NavBar extends PureComponent {
   render() {
     return (
       <div className='navbar'>
-        {/* <ul>
-            <li>
-                <a>Home</a>
-            </li>
-            <li>
-                <a>About</a>
-            </li>
-            <li>
-                <a>Projects</a>
-            </li>
-        </ul> */}
       </div>
     )
   }
